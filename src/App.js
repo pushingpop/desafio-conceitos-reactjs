1f6cc093902bf49a68c6566e2ec656c2d09eb439
Raphael Dantas <dantas@yooh.com.br>
2020-04-16 01:22:04 -0300

    feature: add routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, [])
+
   async function handleAddRepository() {
     // TODO
+    const setTitle = document.getElementById('repository-text').value;
+    console.log(setTitle);
+    
+    const response = await api.post('repositories', {
+      title: setTitle,
+      owner: "Raphael Dantas"
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    api.delete(`repositories/${id}`).then(response => {
+      setRepositories(repositories.filter(repository => repository.id !== id))
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => [
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ])}
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <div className="add__repository">
+        <input type="text" name="" id="repository-text"/>
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </div>
     </div>
   );
 }
